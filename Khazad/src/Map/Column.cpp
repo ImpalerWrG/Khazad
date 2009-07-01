81cdf04c57ff8f917e883417f451c5e23a6c76cb
impalerwrg <impalerwrg@92ebe137-bc98-4eae-8254-15547919f41f>
2009-07-01 21:08:16 +0000

    Added Column file that I forgot to commit last revision

---
+++ Khazad/src/Map/Column.cpp
@@ -0,0 +1,67 @@
+#include <stdafx.h>
+
+#include <Column.h>
+
+Column::Column()
+{
+    XLocation = 0;
+    YLocation = 0;
+}
+
+Column::~Column()
+{
+    for(Uint32 i = 0; i < NegativeVector.size(); i++)
+    {
+        delete NegativeVector[i];
+    }
+
+    for(Uint32 i = 0; i < PositiveVector.size(); i++)
+    {
+        delete PositiveVector[i];
+    }
+}
+
+bool Column::Init(Sint16 X, Sint16 Y)
+{
+    XLocation = X;
+    YLocation = Y;
+}
+
+Cell* Column::getCell(Sint16 Zlevel)
+{
+    if(Zlevel < 0)
+    {
+        if(Zlevel < NegativeVector.size())
+        {
+            return NegativeVector[(Zlevel * -1) - 1];
+        }
+    }
+    else
+    {
+        if(Zlevel < PositiveVector.size())
+        {
+            return PositiveVector[Zlevel];
+        }
+    }
+    return NULL;
+}
+
+void Column::PushCell(Cell* NewCell, Sint16 Zlevel)
+{
+    if(Zlevel < 0)
+    {
+        if(Zlevel > NegativeVector.size())
+        {
+            NegativeVector.resize(Zlevel, NULL);
+        }
+        NegativeVector[(Zlevel * -1) - 1] = NewCell;
+    }
+    else
+    {
+        if(Zlevel >= PositiveVector.size())
+        {
+            PositiveVector.resize(Zlevel + 1, NULL);
+        }
+        PositiveVector[Zlevel] = NewCell;
+    }
+}
